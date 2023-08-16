a5f3e6c4ee2babd5b9cec18cc6238eb9cb047859
linapapadakis <lina.papadakis@icloud.com>
2023-08-16 10:21:30 -0400

    Initial Commit

---
+++ homeScreen/homeScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  homeScreenApp.swift
+//  homeScreen
+//
+//  Created by scholar on 8/16/23.
+//
+
+import SwiftUI
+
+@main
+struct homeScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
